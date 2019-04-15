c80c778360180f55b043c507bef976c3d89ac646
dani0001414 <dani0001414@gmail.com>
2019-04-16 01:01:38 +0200

    newSite

---
+++ streamerdata.js
@@ -0,0 +1,14 @@
+/**MobilBarát Menetrend Testreszabása. */
+/*Streamer adatok megadása*/
+
+//A twitch neved amivel a csatorna elérhető --> http://twitch.tv/twitchneved.
+var streamer = "gige";
+//Kép megjelenítése amikor üres a menetrend.
+var noEventsPic = "icon.png";
+//Kép megjelenítése amikor offline a menetrend(nembiztos, hogy megjelenik majd offline állapotban)
+var offlinePic = "icon.png";
+//Offline állapotban megjelenő szöveg
+var offlineText = "Internet Üzemen kívül! Hebregáld vissza!"; 
+//Üres menetrendkor megjelenő szöveg
+var noEventsText = "A menetrend jelenleg üres!";
+
